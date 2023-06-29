08f3a59e083aab8e9a95c1a359e296f9530d372b
tck <toby3@gmx.de>
2023-06-29 13:52:11 +0200

    [task] switch paths config from string to array

--- config/module.config.php
+++ config/module.config.php
@@ -4,7 +4,9 @@ return array(
         'driver' => array(
             'zfcuser_entity' => array(
                 'class' => 'Doctrine\ORM\Mapping\Driver\XmlDriver',
-                'paths' => __DIR__ . '/xml/zfcuser'
+                'paths' => [
+                    __DIR__ . '/xml/zfcuser',
+                ],
             ),
 
             'orm_default' => array(
